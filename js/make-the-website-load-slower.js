a136501fd6538144b3b2dc78b2dcbe93be71805f
Malte Ubl <malte.ubl@gmail.com>
2013-04-20 12:53:38 -0700

    Decreasing website performance.

---
+++ js/make-the-website-load-slower.js
@@ -0,0 +1,16 @@
+$(function() {
+  var numberOfBackgrounds = 0;
+  var rules = document.styleSheets[1].cssRules;
+  for (var i = 0; i < rules.length; i++) {
+    if (/\.background\d+/.test(rules[i].cssText)) {
+      numberOfBackgrounds++;
+    }
+  }
+  $('link[rel=canonical]').each(function() {
+    var sum = 0;
+    for (var i = 0; i < this.href.length; i++) {
+      sum += this.href.charCodeAt(i);
+    }
+    $(document.body).addClass('background' + (sum % numberOfBackgrounds));
+  });
+});
