b70be3bbcbdf01236d3b87a500e08b52f26cd358
Malte Ubl <malte.ubl@gmail.com>
2013-08-03 10:05:43 -0700

    Script do to the push from the _site directory. Don't forget to pull into _site before starting any work. CC @janl @theophani

---
+++ push.bash
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "cd ./_site"
+cd ./_site
+pwd
+echo "Restore CNAME"
+git checkout c111da4f3c1d92b73dc1023ff3a25fa375e939b9 CNAME
+echo "Commit"
+git commit -a -m Push
+echo "Push"
+git push origin gh-pages
